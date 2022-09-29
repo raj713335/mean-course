04f75efb3b4beb12af36353a90d09167b057979d
Arnab Das <raj713335@gmail.com>
2022-09-30 03:13:59 +0530

    Section 7: Adding Pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { HttpClientModule } from "@angular/common/http";
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -11,25 +11,27 @@ import { Router } from "@angular/router";
 export class PostsService {
 
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>();
+  private postsUpdated = new Subject<{ posts: Post[], postCount: number }>();
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getPosts() {
-    this.http.get<{message: String, posts: any}>('http://localhost:3000/api/posts')
+  getPosts(postsPerPage: number, currentPage: number) {
+    const queryParams = `?pageSize=${postsPerPage}&page=${currentPage}`;
+    this.http.get<{message: String, posts: any, maxPosts: number}>('http://localhost:3000/api/posts' + queryParams)
     .pipe(map((postData) => {
-    return postData.posts.map(post => {
+    return { posts: postData.posts.map(post => {
       return {
         title : post.title,
         content: post.content,
         id: post._id,
         imagePath: post.imagePath
       };
-    });
+    }), maxPosts: postData.maxPosts
+  };
     }))
-    .subscribe((transformedPosts) => {
-      this.posts = transformedPosts;
-      this.postsUpdated.next([...this.posts]);
+    .subscribe((transformedPostsData) => {
+      this.posts = transformedPostsData.posts;
+      this.postsUpdated.next({ posts: [...this.posts], postCount: transformedPostsData.maxPosts });
     });
   }
 
@@ -52,9 +54,7 @@ export class PostsService {
     // const post: Post = { id: null, title: title, content: content};
     this.http.post<{message: string, post : Post}>('http://localhost:3000/api/posts', postData)
     .subscribe(responseData => {
-      const post: Post = { id: responseData.post.id, title: title, content: content, imagePath: responseData.post.imagePath};
-      this.posts.push(post);
-      this.postsUpdated.next([...this.posts]);
+
       this.router.navigate(["/"]);
     });
 
@@ -74,23 +74,12 @@ export class PostsService {
 
     this.http.put("http://localhost:3000/api/posts/" + id, postData)
     .subscribe(response => {
-      const updatedPosts = [...this.posts];
-      const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
-      const post: Post = { id: id, title: title, content: content, imagePath: "response.imagePath" };
-      updatedPosts[oldPostIndex] = post;
-      this.posts = updatedPosts;
-      this.postsUpdated.next([...this.posts]);
       this.router.navigate(["/"]);
     });
   }
 
   deletePost(postId: string){
-    this.http.delete('http://localhost:3000/api/posts/' + postId)
-    .subscribe(() => {
-      const updatedPosts = this.posts.filter(post => post.id !== postId);
-      this.posts = updatedPosts;
-      this.postsUpdated.next([...this.posts]);
+    return this.http.delete('http://localhost:3000/api/posts/' + postId);
 
-    });
   }
 }
